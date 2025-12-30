0000aa55d62d9096e0a98b96e378fb43f0c8e6d7
pitsinee-6721652421 <pitsinee.ka@ku.th>
2025-12-30 11:05:30 +0700

    add js

---
+++ js/main.js
@@ -0,0 +1,127 @@
+//console.log("Hello world")
+//console.log("this is a tast Java file")
+
+
+//string number boolen Null Object Array
+//รูปแบบการประกาศตัวแปร 
+// etสามารถปรับเปลี่ยนค่าได้
+/**let fname = "John";  //string
+console.log("Name: ",fname);
+const PI=3.14;  //const ค่าคงที่ ประกาศค่าไหน เก็บค่านั้น
+
+let age =30;//number
+let height=5.9;
+
+//สามารถปรับเปลี่ยนค่าได้
+fname = "Fanta";
+
+console.log("Name: ",fname);
+console.log("Age: ",age);
+console.log("Height:",height)
+
+/**
++ บวก
+- ลบ
+* คูณ
+ / หาร
+ %หารเอาเศษ
+ 
+
+ let number1 = 10;  //'10' จะเป็นstring
+ let number2 = 3;   //'3' จะเป็นstring จะไม่นำมาบวกจะจะต่อกันเป็น 103
+
+ let resulit = number1 + number2
+ console.log("ผลบวก =",resulit)
+
+
+ == เท่ากับ
+ =! ไม่เท่ากับ
+ > มากกว่า
+ < น้อยกว่า
+ >= มากกว่าหรือเท่ากับ
+ <= น้องกว่าหรือเท่ากับ
+*/
+ 
+
+//let number1 = 5;
+//let number2 = 5;
+//let condition number1 < number2 ;
+//console.log("Congitio:",conditio); 
+
+
+// if -else condition
+/**if(number1 > number2) {
+    console.log("this  is if!!")
+} else if (number1 < number2){
+    console.log("this  is else if!!")
+}else{
+    console.log("this  is else!!")
+}
+*/
+
+//คำนวณเกรด
+//let  score = 75 ;
+/**
+ * 
+
+let  score = prompt
+if (score >= 80){
+    console.log(" เกรด A");
+}else if (score >= 70){
+    console.log("เกรด B");
+}else if (score >= 60){
+    console.log("เกรด C")
+}else if (score >=50){
+    console.log("เกรด D")
+}else{
+    console.log("เกรด F!!")
+}
+ */
+
+/**
+ * && และ
+* || หรือ
+* ! not หรือ ไม่
+  */ 
+
+/**let  number1 = 5
+let number2 = 10
+
+let conditio1 = (number1 > 0) && (number2>0) // true && true = true
+console.log("conditio1: ",conditio1)
+let conditio2 = (number1 > 0) || (number2>0) // true && true = true
+
+
+let age = 25
+let gender = "femaie"
+if (age >= 18 && gender == "femaie"){
+    console.log ="คุณสามารถเข้าได้"
+}
+    */
+
+/**
+ * 
+
+let number1 =20
+if (!(number1 % 2 == 0)){
+    console.log("Even")
+}
+else{
+    console.log("Odd")
+}
+ */
+
+/**
+ * Loop
+ * ehile
+ * for
+ */
+let conter = 0
+while (conter <= 4){
+ conter+=1
+ console.log("while",conter)
+}
+
+for (let i =0; i <=4 ; i= i + 1){
+    console.log("for:",i);
+}
