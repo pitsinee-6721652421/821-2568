e08827f67fb42d418e88b0bd75f574ede927a6e0
pitsinee-6721652421 <pitsinee.ka@ku.th>
2026-02-17 11:40:08 +0700

    api server

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,60 @@
-//27/1/69     // ทำการ import http module เพื่อสร้าง server
-const http = require('http');
-const host = 'localhost';
+//17/02/69
+c/nst express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 const port = 8000;
+app.use(bodyParser.json());
+let users = []
+let counter = 1;
 
-// กำหนดค่าเริ่มต้นของ server เมื่อเปิดใช้งาน เว็บผ่านเบราว์เซอร์ ที่ localhost:8000
-const requestListener = function (req, res) { //มีคำร้อง
-    res.writeHead(200);
-    res.end('My First Server!');
-}
+// path = GET /users
+app.get('/users', (req, res) => {
+    res.json(users);
+});
 
-// run server
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running at http://${host}:${port}`);
+// path = POST /user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter;
+    counter += 1;
+    users.push(user);
+    res.json({
+        message: 'User added successfully',
+        user: user
+    });
+});
+// path = PUT /user/:id  //แก้
+app.put('/user/:id', (req, res) => {
+    
+    let updatedUser = req.body;
+    let id = req.params.id;
+    //user จาก id ที่ส่งมา
+    let selectedIndex = users.findIndex(user => user.id == id);
+     if (updatedUser.name) {
+        users[selectedIndex].name = updatedUser.name    
+    }
+    if (updatedUser.age) {
+        users[selectedIndex].age = updatedUser.age
+    }
+
+    //อัพเดทข้อมูล user
+    users[selectedIndex] = updatedUser
+
+    //เอาข้อมูลที่ update ส่ง response กลับไป
+    res.json({
+        message: 'User updated successfully',
+        data: {
+            user: updatedUser,
+            indexUpdated: selectedIndex
+        }
+    })
+});
+
+app.delete("/user/:id", (req, res) => {
+    let selectedIndex = users.findIndex(users => users .id ==id);
+    delete users[selectedIndex];
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
