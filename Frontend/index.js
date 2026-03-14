33ce3398889978754527a9a2ba89da149a0203d7
pitsinee-6721652421 <pitsinee.ka@ku.th>
2026-03-14 17:26:39 +0700

    add.

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,3 +1,52 @@
+const BASE_URL = 'http://localhost:8000';
+
+let mode = 'create';
+let selectedId = ''
+window.onload = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    console.log('id', id);
+    if (id) {
+        mode = 'edit';
+        selectedId = id;
+        //1. ดึงข้อมูล user ออกมา 
+        try{
+            const response = await axios.get(`${BASE_URL}/users/${id}`);
+            const user = response.data;
+            console.log('response', response.data);
+
+    //2. นำข้อมูลที่ได้มาแสดงใน form เพื่อให้ผู้ใช้สามารถแก้ไขข้อมูลได้
+    let firstNameDOM = document.querySelector('input[name=firstname]');
+    let lastNameDOM = document.querySelector('input[name=lastname]');
+    let ageDOM = document.querySelector('input[name=age]');
+    let descriptionDOM = document.querySelector('textarea[name=description]');
+    firstNameDOM.value = user.firstname;
+    lastNameDOM.value = user.lastname;
+    ageDOM.value = user.age;
+    descriptionDOM.value = user.description;
+
+
+
+    let genderDOM = document.querySelectorAll('input[name=gender]')
+    let interestDOMs = document.querySelectorAll('input[name=interests]') 
+    for (let i = 0; i < genderDOM.length; i++) {
+        if (genderDOM[i].value === user.gender) {
+            genderDOM[i].checked = true;
+        }
+    }
+    for (let i = 0; i < interestDOMs.length; i++) {
+        if (user.interests.includes(interestDOMs[i].value)) {
+            interestDOMs[i].checked = true;
+        }
+    }
+
+             
+        }catch(error){
+            console.error('Error fetching user data:', error);
+        }
+
+}
+
 const validateData = (userData) => {
     let errors = [];
     if (!userData.firstname) {
@@ -56,11 +105,30 @@ const submitData = async () => {
                errors: errors
             }
        }
+       let message = 'บันทึกข้อมูลสำเร็จ';
+          if (mode === 'create') {
+            const response = await axios.post(`${BASE_URL}/users`, userData);
+            console.log('response', response.data);
+          } else if (mode === 'edit') {
+            const response = await axios.put(`${BASE_URL}/users/${selectedId}`, userData);
+            message = 'เเก้ไขข้อมูลสำเร็จ';
+            console.log('response', response.data);
+          }
+
+
 
         const response = await axios.post('http://localhost:8000/users', userData)
         console.log('response', response.data);
         messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ'
         messageDOM.className = 'message success'
+        if (mode === 'create') {
+            const response = await axios.post(`${BASE_URL}/users`, userData);
+            console.log('response', response.data);
+          } else if (mode === 'edit') {
+            const response = await axios.put(`${BASE_URL}/users/${selectedId}`, userData);
+            message = 'เเก้ไขข้อมูลสำเร็จ';
+            console.log('response', response.data);
+          }
 
     } catch (error) {
         console.log('error message', error.message);
@@ -84,4 +152,4 @@ const submitData = async () => {
         messageDOM.innerHTML = htmlData
         messageDOM.className = 'message danger'
     }
-}
+}}
--- Frontend/user.js
+++ Frontend/user.js
@@ -1,42 +1,94 @@
-//1. load user ทั้งหมดจาก api /users
+//1. load user ทั้งหมดจาก api
 const BASE_URL = 'http://localhost:8000';
+
+let mode = 'create';
+let selectedId = '';
+
 window.onload = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    console.log('id', id);
+//1. ดึงข้อมูล user ออกมา
+    if (id) {
+        mode = 'edit';
+        selectedId = id;
+
+        try {
+            const response = await axios.get(`${BASE_URL}/users/${id}`);
+            const user = response.data;
+            console.log('response', user);
+
+//2. นำข้อมูลที่ได้มาแสดงใน form เพื่อให้ผู้ใช้สามารถแก้ไขข้อมูลได้
+            let firstNameDOM = document.querySelector('input[name=firstname]');
+            let lastNameDOM = document.querySelector('input[name=lastname]');
+            let ageDOM = document.querySelector('input[name=age]');
+            let descriptionDOM = document.querySelector('textarea[name=description]');
+
+            firstNameDOM.value = user.firstname;
+            lastNameDOM.value = user.lastname;
+            ageDOM.value = user.age;
+            descriptionDOM.value = user.description;
+
+            let genderDOM = document.querySelectorAll('input[name=gender]');
+
+            for (let i = 0; i < genderDOM.length; i++) {
+                if (genderDOM[i].value === user.gender) {
+                    genderDOM[i].checked = true;
+                }
+            }
+            for (let i = 0; i < interestDOMs.length; i++) {
+                if (user.interests.includes(interestDOMs[i].value)) {
+                  interestDOMs[i].checked = true;
+        }
+    } 
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    await loadData();
 }
-const loadData = async () =>{
-   
-  const response = await axios.get(`${BASE_URL}/users`);
-  console.log(response.data);
 
-  const userDOM = document.getElementById('user');
-  let htmlData = `<div>`;
 
-  //2. นำข้อมูล user ที่ได้มาแสดงในหน้าเว็บ
-  for (let i = 0; i < response.data.length; i++) {
-    let user = response.data[i];
-    htmlData += `<div>
-      ${user.id} ${user.firstname} ${user.lastname}
-      <button>Edit</button>
-      <button class='delete' data-id='${user.id}'>Delete</button>
-    </div>`;
-  }
 
-  htmlData += `</div>`;
-  userDOM.innerHTML = htmlData;
 
-  const deleteDOMs = document.getElementsByClassName('delete');
+const loadData = async () => {
+    const response = await axios.get(`${BASE_URL}/users`);
+    console.log(response.data);
 
-  for (let i = 0; i < deleteDOMs.length; i++) {
-    deleteDOMs[i].addEventListener("click", async (event) => {
-      const id = event.target.dataset.id;
+    const userDOM = document.getElementById('user');
+    let htmlData = '<div>';
 
-      try {
-        //1.
-        await axios.delete(`${BASE_URL}/users/${id}`);
-        location.reload(); // recurisev
-        //2.
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  }
+    // แสดง user ทั้งหมด
+    for (let i = 0; i < response.data.length; i++) {
+        let user = response.data[i];
+
+        htmlData += `
+        <div>
+            ${user.id} ${user.firstname} ${user.lastname}
+            <a href="index.html?id=${user.id}">
+                <button>Edit</button>
+            </a>
+            <button class="delete" data-id="${user.id}">Delete</button>
+        </div>
+        `;
+    }
+
+    htmlData += '</div>';
+    userDOM.innerHTML = htmlData;
+
+    const deleteDOMs = document.getElementsByClassName('delete');
+
+    for (let i = 0; i < deleteDOMs.length; i++) {
+        deleteDOMs[i].addEventListener('click', async (event) => {
+            const id = event.target.dataset.id;
+
+            try {
+                await axios.delete(`${BASE_URL}/users/${id}`);
+                loadData(); // reload data
+            } catch (error) {
+                console.error(error);
+            }
+        });
+    }
 }
